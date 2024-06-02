9699352ae43816c55aee0f503cb1cbdf2c9a0af1
jappun <jappun@student.ubc.ca>
2024-06-01 18:29:31 -0700

    make API calls, update readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# wod-chrome
-Word of the Day Chrome extension to expand my vocabulary.
+# Dictionary Chrome Extension
+(WIP)
+I often come across new words while reading online. I look them up but quickly forget them, so I'm building this Chrome Extension to jot them down.
+
+### How to Use
+I don't plan to publish this to the Chrome Store. However, you are welcome to download it and use it! This section will be updated with instructions once the features are built out.
+
+### Currently Working On
+- building the dictionary
+    - save word definitions and your own notes on the word.
+
+### Possible Future Features
+- build a quiz on words in your dictionary
+- flashcards
---
+++ src/app.js
@@ -0,0 +1,26 @@
+var API_KEY= "YOUR-API-KEY-HERE";
+
+document.addEventListener("DOMContentLoaded", function() {
+    // search new word
+    document.getElementById("submit").addEventListener("click", submit);
+    // To view your entire dictionary
+    document.getElementById("view").addEventListener("click", viewDict);
+
+    function viewDict(){
+        location.href="dict.html";
+
+    }
+    function submit() {
+        var newWord = document.getElementById("newWord").value;
+        // todo: change source dictionary
+        // get the top result
+        var req = `https://api.wordnik.com/v4/word.json/${newWord}/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=${API_KEY}`;
+
+        fetch(req)
+        .then(response => {return response.json()})
+        .then(data => {console.log(data);})
+        .catch(error => {
+            console.error('Error fetching definitions:', error);
+        });
+    }
+});
